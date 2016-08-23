94fb216d8ee0504146f619f0a7086543cf12d38b
Don Kingery <Don Kingery>
2016-08-23 11:55:51 -0400

    2nd commit

--- Print_to_Screen.rb
+++ Print_to_Screen.rb
@@ -15,7 +15,7 @@
 
 number = 1
 100.times do
-if number == 3
+if number % 3 == 0
 	puts "Mined"
 else
 	puts number
@@ -23,15 +23,7 @@ end
 number = number + 1
 end
 
-number = 1
-100.times do
-if number == {x / 3}
-	puts "Mined"
-else
-	puts number
-end
-number = number + 1
-end
+
 
 
 #if {x / 3}  = Mined
