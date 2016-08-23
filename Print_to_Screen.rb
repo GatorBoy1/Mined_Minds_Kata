d17f039112b7a47979e6a11aa11c8e85a4e2e4a1
Don Kingery <Don Kingery>
2016-08-23 11:40:30 -0400

    first commit

---
+++ Print_to_Screen.rb
@@ -0,0 +1,43 @@
+# "1" * 100 #will this work
+
+#100.times do
+#	puts 2
+#end
+
+
+
+#print 1.step(100).to_a
+
+#(1..100).each do |whatever|
+#	puts whatever
+#	 end
+
+
+number = 1
+100.times do
+if number == 3
+	puts "Mined"
+else
+	puts number
+end
+number = number + 1
+end
+
+number = 1
+100.times do
+if number == {x / 3}
+	puts "Mined"
+else
+	puts number
+end
+number = number + 1
+end
+
+
+#if {x / 3}  = Mined
+#if {x / 5}  = Minds
+#if {x / 3 and x / 5} = Mined Minds
+
+ 
+
+
